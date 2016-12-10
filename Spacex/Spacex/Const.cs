e570e41821fbaab4f93b441ff127ad593aa7fd84
imicadio <mrjesio@gmail.com>
2016-12-10 21:14:27 +0100

    Tytuł i Stałe

---
+++ Spacex/Spacex/Const.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spacex
+{
+    class Const
+    {
+        public static int GAME_WIDTH = 400;
+        public static int GAME_HEIGHT = 600;
+        public static string TITLE = "Spacex";
+    }
+}
--- Spacex/Spacex/Spacex.cs
+++ Spacex/Spacex/Spacex.cs
@@ -14,23 +14,21 @@ namespace Spacex
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Spacex : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public Spacex()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            this.graphics.PreferredBackBufferHeight = Const.GAME_HEIGHT;
+            this.graphics.PreferredBackBufferWidth = Const.GAME_WIDTH;
+            this.Window.Title = Const.TITLE;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here

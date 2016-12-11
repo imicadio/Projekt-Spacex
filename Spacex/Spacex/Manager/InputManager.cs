fda7e5b2262ad2f8f8595feb790a426b7d27acf9
imicadio <mrjesio@gmail.com>
2016-12-11 21:27:42 +0100

    Komentarze InputManager

--- Spacex/Spacex/Manager/InputManager.cs
+++ Spacex/Spacex/Manager/InputManager.cs
@@ -8,23 +8,23 @@ namespace Spacex.Manager
 {
     public class InputManager
     {
-        private KeyboardState _oldKS;
-        private KeyboardState _KS;
+        private KeyboardState _oldKS;   // poprzedni stan  
+        private KeyboardState _KS;      // obecny stan
 
         public InputManager()
         {
-            Const.INPUT = this;
+            Const.INPUT = this;     // obecne wywołanie InputManager
         }
 
         public void Update()
         {
-            if (_KS != null)
+            if (_KS != null)    // cokolwiek jest przypisane do zmiennej KS w chwili obecnej to poprzedni stan KS jest równy obecnemu stanowi KS
                 _oldKS = _KS;
 
-            _KS = Keyboard.GetState();
+            _KS = Keyboard.GetState();  // przypisanie wartosci z obecnego stanu wciśnięcia przycisku (gdy nic nie jesy wciskane, przypisuje się null)
         }
 
-        public bool isKeyPressed(Keys k)
+        public bool isKeyPressed(Keys k)    // funkcja sprawdza czy dany przycisk jako argument funkcji (z klasy Keys) został właśnie wciśnięty, przy czym w poprzedniej klatce ten sam klawisz jest odciśnięty (Up)
         {
             return (_oldKS.IsKeyUp(k) && _KS.IsKeyDown(k));
         }
